28269fecc7475249533b9fc42143777673275fa9
Catherine Lim <cathlim.98@gmail.com>
2023-09-02 16:33:26 +0800

    Add reverseLinkedList.py

---
+++ python/LinkedList/reverseLinkedList.py
@@ -0,0 +1,35 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+#Iterative method
+#Time complexity: O(n)
+#Space complexity: O(1)
+"""
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        prev, curr = None, head
+
+        while curr:
+            nxt = curr.next
+            curr.next = prev
+            prev = curr
+            curr = nxt
+        return prev
+"""
+#Recursive method
+#Time complexity: O(n)
+#Space complexity: O(n), as the size of linked-list directly proportional to the recursive times
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if not head:
+            return None
+        
+        newHead = head
+        if head.next:
+            newHead = self.reverseList(head.next)
+            head.next.next = head
+            head.next = None
+        return newHead
