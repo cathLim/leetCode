2819363c1104d8a98377b83ecc1450f9c61ed3d8
Catherine Lim <cathlim.98@gmail.com>
2023-09-03 18:10:05 +0800

    Add leetcode ques maxDepthOfBinaryTree.py

---
+++ python/Trees/maxDepthOfBinaryTree.py
@@ -0,0 +1,54 @@
+#https://leetcode.com/problems/maximum-depth-of-binary-tree/submissions/
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+#With Recursive DFS - in order DFS
+"""
+class Solution:
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+        #Base Case
+        if not root:
+            return 0
+
+        return 1 + max(self.maxDepth(root.left), self.maxDepth(root.right))
+"""
+"""
+#BFS 
+class Solution:
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+        #Base Case
+        if not root:
+            return 0
+
+        level = 0
+        q = deque([root])
+        while q:
+            for i in range(len(q)):
+                node = q.popleft()
+                if node.left:
+                    q.append(node.left)
+                if node.right:
+                    q.append(node.right)
+            level += 1
+        return level
+"""
+
+#Iterative DFS - pre order DFS
+class Solution:
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+        stack = [[root, 1]]
+        res = 0
+
+        while stack:
+            node, depth = stack.pop()
+            
+            if node:
+                res = max(res, depth)
+                stack.append([node.left, depth + 1])
+                stack.append([node.right, depth + 1])
+        return res
+        
