852d6c8ddec7ac18fc9c3eb155d36f6dc14a828b
Catherine Lim <cathlim.98@gmail.com>
2023-09-08 22:55:02 +0800

    Add leetcode ques subsets

---
+++ python/Backtracking/subsets.py
@@ -0,0 +1,22 @@
+#https://leetcode.com/problems/subsets/
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        res = []
+        subset = []
+
+        def dfs(i):
+            if i >= len(nums):
+                res.append(subset.copy())
+                return
+
+            #decision to include num[i]
+            subset.append(nums[i])
+            dfs(i + 1)
+
+            #decision Not to include num[i]
+            subset.pop()
+            dfs(i + 1)
+        
+        dfs(0)
+        return res
+        
