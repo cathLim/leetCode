12aad60ae2872f063066c8691c50af7d22cf0b02
Catherine Lim <leeping213@hotmail.com>
2023-09-23 23:37:18 +0800

    Reupdate leetcode ques postOrderTraversal.py

---
+++ python/Trees/postOrderTraversal.py
@@ -0,0 +1,20 @@
+#https://leetcode.com/problems/binary-tree-postorder-traversal/
+class Solution:
+    def postorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        stack = [root]
+        visit = [False]
+        res = []
+
+        while stack:
+            cur, v = stack.pop(), visit.pop()
+            if cur:
+                if v:
+                    res.append(cur.val)
+                else:
+                    stack.append(cur)
+                    visit.append(True)
+                    stack.append(cur.right)
+                    visit.append(False)
+                    stack.append(cur.left)
+                    visit.append(False)
+        return res
