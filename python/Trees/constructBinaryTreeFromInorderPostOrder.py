15114ab4b99961d33ba2a49e1fe88a9e615a1022
Catherine Lim <leeping213@hotmail.com>
2023-10-13 00:18:35 +0800

    Add leetcode ques

---
+++ python/Trees/constructBinaryTreeFromInorderPostOrder.py
@@ -0,0 +1,23 @@
+#https://leetcode.com/problems/construct-binary-tree-from-inorder-and-postorder-traversal/description/
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def buildTree(self, inorder: List[int], postorder: List[int]) -> Optional[TreeNode]:
+        inorderIndex = {v : i for i, v in enumerate(inorder)}
+
+        def helper(l, r):
+            if l > r:
+                return None
+            
+            root = TreeNode(postorder.pop())
+
+            idx = inorderIndex[root.val]
+            root.right = helper(idx + 1, r)
+            root.left = helper(l, idx - 1)
+            return root
+        
+        return helper(0, len(inorder) - 1)
