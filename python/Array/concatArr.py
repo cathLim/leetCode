9a95cd6c2f7042c79339bbcd83702ba3aa76fb55
Catherine Lim <leeping213@hotmail.com>
2023-10-31 00:27:23 +0800

    Add leetcode ques

---
+++ python/Array/concatArr.py
@@ -0,0 +1,13 @@
+#https://leetcode.com/problems/concatenation-of-array/
+class Solution(object):
+    def getConcatenation(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        ans = []
+        
+        for i in range(2):
+            for n in nums:
+                ans.append(n)
+        return ans
